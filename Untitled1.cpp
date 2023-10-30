97b7f26fe73e6dc3e77e254e70c1e7dcc7620da1
Tstrz18 <148056269+Tstrz18@users.noreply.github.com>
2023-10-30 12:01:11 +0100

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std; 
+
+int main()
+{
+	short a, b;
+	cout <<"Podaj A:";
+	cin >> a;
+	cout <<"Podaj B:";
+	cin >> b;
+	if(a>b)
+		cout <<"a > b ";
+	[
+	
+	]	{};if else 
+	cout <<"a < b";
+}
